7724e71f94b3a2d55058859334c07d0c7e17aff1
VAS-18 <uday.vas18@gmail.com>
2025-03-21 19:47:47 +0530

    Add initial components and layout for co-manga application

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+export const metadata = {
+  title: 'Home'
+}
+
+const HomePage = () => {
+  return ( 
+    <Button className="bg-white text-black">
+      HELLO
+    </Button>
+   );
+}
+ 
+export default HomePage;
