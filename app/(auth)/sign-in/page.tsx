49a1dfebec2bb8f7e5470c01dbc369cd02e89f2c
VAS-18 <uday.vas18@gmail.com>
2025-03-26 06:37:21 +0530

    Add authentication layout and sign-in form components; update constants and package dependencies

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,58 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import SignInForm from "./sign-in-form";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string
+  }>
+}) => {
+
+  const {callbackUrl} = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src={"/image/logo.png"}
+              alt="logo"
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <SignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,9 @@
-export const APP_NAME = 'co-manga'
+export const APP_NAME = "co-manga";
 
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/"
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/";
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
